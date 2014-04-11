a2db5678190c0155a5a95dd54833b86e0fc4a7ab
Dieter Plaetinck <dieter@vimeo.com>
2014-04-11 11:05:48 -0400

    add some [debug] logging

--- graphite_influxdb.py
+++ graphite_influxdb.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 try:
     from graphite_api.intervals import Interval, IntervalSet
@@ -30,18 +31,19 @@ def config_to_client(config=None):
 
 
 class InfluxdbReader(object):
-    __slots__ = ('path', 'client')
+    __slots__ = ('client', 'path', 'logger')
 
-    def __init__(self, client, path):
+    def __init__(self, client, path, logger):
         self.client = client
         self.path = path
+        self.logger = logger
 
     def fetch(self, start_time, end_time):
         data = self.client.query("select time, value from %s where time > %ds and time < %ds order asc" % (self.path, start_time, end_time))
         datapoints = []
         start = 0
         end = 0
-        step = 0
+        step = 10
         try:
             points = data[0]['points']
             start = points[0][0]
@@ -51,6 +53,8 @@ class InfluxdbReader(object):
         except Exception:
             pass
         time_info = start, end, step
+        self.logger.debug("influx REQUESTED RANGE for %s: %d to %d" % (self.path, start_time, end_time))
+        self.logger.debug("influx RETURNED  RANGE for %s: %d to %d" % (self.path, start, end))
         return time_info, datapoints
 
     def get_intervals(self):
@@ -67,24 +71,34 @@ class InfluxdbReader(object):
 
 
 class InfluxdbFinder(object):
-    __slots__ = ('client')
+    __slots__ = ('client', 'logger')
 
     def __init__(self, config=None):
         self.client = config_to_client(config)
+        # from graphite_api.app import app
+        # self.logger = app.logger
+        logging.basicConfig()
+        self.logger = logging.getLogger("graphite-influxdb")
+        self.logger.setLevel(logging.DEBUG)
 
     def find_nodes(self, query):
         # query.pattern is basically regex, though * should become [^\.]+ and . \.
         # but list series doesn't support pattern matching/regex yet
         regex = query.pattern.replace('.', '\.').replace('*', '[^\.]+')
+        self.logger.info("find_nodes query: %s -> %s" % (query.pattern, regex))
         regex = re.compile(regex)
         series = self.client.query("list series")
+        for s in series:
+            self.logger.info("matching %s" % s['name'])
         series = [s['name'] for s in series if regex.match(s['name']) is not None]
         seen_branches = set()
         # for leaf "a.b.c" we should yield branches "a" and "a.b"
         for s in series:
-            yield LeafNode(s, InfluxdbReader(self.client, s))
+            self.logger.info("leaf %s" % s)
+            yield LeafNode(s, InfluxdbReader(self.client, s, self.logger))
             branch = s.rpartition('.')[0]
             while branch != '' and branch not in seen_branches:
+                self.logger.info("branch %s" % branch)
                 yield BranchNode(branch)
                 seen_branches.add(branch)
                 branch = branch.rpartition('.')[0]
