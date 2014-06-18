18715a65ac6971dce7f80ca825cbd19c75984df4
Dieter Plaetinck <dieter@vimeo.com>
2014-06-18 10:12:49 -0400

    version 0.2!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='graphite-influxdb',
-    version='0.1.3',
+    version='0.2',
     url='https://github.com/vimeo/graphite-influxdb',
     license='apache2',
     author='Dieter Plaetinck',
