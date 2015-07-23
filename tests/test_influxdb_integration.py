1c4cad77e90aaea96a1a8f5cfc3bd7ec255264cb
Panos <panos.kittenis@thomsonreuters.com>
2015-07-23 11:22:17 +0100

    Added fetch single and multi integration tests for getting influxdb data

--- tests/test_influxdb_integration.py
+++ tests/test_influxdb_integration.py
@@ -1,7 +1,6 @@
 import unittest
 import graphite_influxdb
 import datetime
-import random
 
 class Query(object):
 
@@ -11,6 +10,7 @@ class Query(object):
 class GraphiteInfluxdbIntegrationTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.step, self.num_datapoints = 60, 2
         self.config = { 'influxdb' : { 'host' : 'localhost',
                                        'port' : 8086,
                                        'user' : 'root',
@@ -20,6 +20,9 @@ class GraphiteInfluxdbIntegrationTestCase(unittest.TestCase):
                                        'log_level' : 'debug',
                                        }}
         self.finder = graphite_influxdb.InfluxdbFinder(self.config)
+        self.metric_prefix = "integration_test"
+        self.series1, self.series2 = ".".join([self.metric_prefix, "leaf_node1"]), \
+          ".".join([self.metric_prefix, "leaf_node2"])
 
     def test_find_series(self):
         """Test finding a series by name"""
@@ -42,3 +45,58 @@ class GraphiteInfluxdbIntegrationTestCase(unittest.TestCase):
         expected = ['leaf_node1', 'leaf_node2']
         self.assertEqual(nodes, expected,
                          msg="Expected leaf node list '%s' - got %s" % (expected, nodes))
+
+    def test_multi_fetch_data(self):
+        """Test fetching data for a single series by name"""
+        nodes = list(self.finder.find_nodes(Query(self.series1)))
+        start_time, end_time = (datetime.datetime.now() - datetime.timedelta(hours=1)), \
+          datetime.datetime.now()
+        time_info, data = self.finder.fetch_multi(nodes,
+                                                  int(start_time.strftime("%s")),
+                                                  int(end_time.strftime("%s")))
+        self.assertTrue(self.series1 in data,
+                        msg="Did not get data for requested series %s - got data for %s" % (
+                            self.series1, data.keys(),))
+        self.assertEqual(time_info,
+                         (int(start_time.strftime("%s")),
+                          int(end_time.strftime("%s")),
+                         self.step),
+                         msg="Time info and step do not match our requested values")
+        datapoints = [v for v in data[self.series1] if v]
+        self.assertTrue(len(datapoints) == self.num_datapoints,
+                        msg="Expected %s datapoints - got %s" % (
+                            self.num_datapoints, len(datapoints),))
+
+    def test_single_fetch_data(self):
+        """Test single fetch data for a series by name"""
+        node = list(self.finder.find_nodes(Query(self.series1)))[0]
+        start_time, end_time = (datetime.datetime.now() - datetime.timedelta(hours=1)), \
+          datetime.datetime.now()
+        time_info, data = node.reader.fetch(int(start_time.strftime("%s")),
+                                            int(end_time.strftime("%s")))
+        datapoints = [v for v in data[self.series1] if v]
+        self.assertTrue(len(datapoints) == self.num_datapoints,
+                        msg="Expected %s datapoints - got %s" % (
+                            self.num_datapoints, len(datapoints),))
+        
+    def test_multi_fetch_data_multi_series(self):
+        """Test fetching data for multiple series by name"""
+        nodes = list(self.finder.find_nodes(Query(self.metric_prefix + ".*")))
+        start_time, end_time = (datetime.datetime.now() - datetime.timedelta(hours=1)), \
+          datetime.datetime.now()
+        time_info, data = self.finder.fetch_multi(nodes,
+                                                  int(start_time.strftime("%s")),
+                                                  int(end_time.strftime("%s")))
+        self.assertTrue(self.series1 in data and self.series2 in data,
+                        msg="Did not get data for requested series %s and %s - got data for %s" % (
+                            self.series1, self.series2, data.keys(),))
+        self.assertEqual(time_info,
+                         (int(start_time.strftime("%s")),
+                          int(end_time.strftime("%s")),
+                         self.step),
+                         msg="Time info and step do not match our requested values")
+        for series in [self.series1, self.series2]:
+            datapoints = [v for v in data[series] if v]
+            self.assertTrue(len(datapoints) == self.num_datapoints,
+                            msg="Expected %s datapoints for series %s - got %s" % (
+                                self.num_datapoints, series, len(datapoints),))
